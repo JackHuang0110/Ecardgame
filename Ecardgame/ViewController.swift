be6b1a7d56ba504fbbad3333ed3f714e52d6737a
Jack Huang <jackhuang@Jackde-MacBook-Air.local>
2018-08-23 13:46:28 +0800

    Initial Commit

---
+++ Ecardgame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ecardgame
+//
+//  Created by Jack Huang on 2018/8/23.
+//  Copyright © 2018 Jack Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
